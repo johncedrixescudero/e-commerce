b289569cb932cbdea66da97f064c25742c0498f8
John Cedrix Escudero <johncedrixescudero@gmail.com>
2023-12-04 10:17:59 +0800

    refractor add react library on ecommerce project

--- ecommerce/src/components/Header.jsx
+++ ecommerce/src/components/Header.jsx
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="output.css">
-</head>
-<body>
+import React from 'react'
 
-    <header class="fixed top-0 w-full bg-slate-50 shadow-md border-b-2 border-slate-100 ">
+const Header = () => {
+  return (
+    <div>
+      <header class="fixed top-0 w-full bg-slate-50 shadow-md border-b-2 border-slate-100 ">
         <div class="mx-4 my-4 flex justify-between ">
           <div>LOGO</div>
           <div class="flex w-3/4">
-            <input type="search" class="border w-3/4">
+            <input type="search" class="border w-3/4"/>
             <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 border bg-white">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
@@ -33,6 +28,8 @@
           </div>
        </div>
     </header>
+    </div>
+  )
+}
 
-</body>
-</html>
+export default Header
--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
